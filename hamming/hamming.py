d0d05bd98cbaf3a15e9800cd7e67a96d5f7204bc
Catarina Lourenco <chbic.medusa@gmail.com>
2019-03-17 12:25:29 +0000

    Hamming distance passing all tests

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,6 +1,8 @@
 def distance(strand_a, strand_b):
     count = 0
-    if(len(strand_a) == len(strand_b)):
+    if(len(strand_a) != len(strand_b)):
+        raise ValueError("Hamming strings must be of the same size")
+    else:
         for i in range(len(strand_a)):
             count += (0 if strand_a[i] == strand_b[i] else 1)
     return count
