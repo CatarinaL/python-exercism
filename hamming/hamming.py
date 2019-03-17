376a63371dc9bd42dd34e1847d7b39a2fde4d294
Catarina Lourenco <chbic.medusa@gmail.com>
2019-03-17 13:04:00 +0000

    Hamming with improvements and language specific functions

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,8 +1,14 @@
 def distance(strand_a, strand_b):
+    """
     count = 0
+    """
     if(len(strand_a) != len(strand_b)):
-        raise ValueError("Hamming strings must be of the same size")
-    else:
-        for i in range(len(strand_a)):
-            count += (0 if strand_a[i] == strand_b[i] else 1)
+        raise ValueError("Hamming strands must be of the same size")
+    #for i in range(len(strand_a)):
+    #    count += (0 if strand_a[i] == strand_b[i] else 1)
+    """
+    for i, j in zip(strand_a, strand_b):
+        count += 0 if i == j else 1
     return count
+    """
+    return sum([1 for i, j in zip(strand_a, strand_b) if i != j])
