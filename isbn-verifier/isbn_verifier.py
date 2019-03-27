3816edd417801355cd8db26ce6459449f241660a
Catarina Lourenco <chbic.medusa@gmail.com>
2019-03-27 10:58:21 +0000

    ISBN tidied up

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,21 +1,18 @@
 import re
 
-
 def verify(isbn):
     isbn_digits = isbn.replace("-", "")
-    return is_valid_format(isbn_digits) and checksum(isbn_digits) == 0
+    return is_valid_format(isbn_digits) and valid_checksum(isbn_digits)
 
 # validate ISBN-10 format - [\dX]*, 10 digits without hyphens
 def is_valid_format(isbn_digits):
-    if re.match(r"[[\d]{9}(X|\d)$", isbn_digits) is None:
-        return False
-    return True
+    return re.match(r"[[\d]{9}(X|\d)$", isbn_digits) is not None
 
 # convert each char to int, if X -> 10
 # checksum
-def checksum(isbn_digits):
+def valid_checksum(isbn_digits):
     digits = zip(isbn_digits[:-1], range(10, 1, -1))
     digits = [int(x) * i for x, i in digits]
-    return (sum(digits) + (10 if isbn_digits[-1] == 'X' else int(isbn_digits[-1]))) % 11
-
+    check_digit = 10 if isbn_digits[-1] == 'X' else int(isbn_digits[-1])
+    return ((sum(digits) + check_digit) % 11) == 0
 
