a40a3834bdfb9f5da7ef639f10ee767b3f629124
Catarina Lourenco <chbic.medusa@gmail.com>
2019-03-27 10:48:23 +0000

    ISBN tidy version passing all tests

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -3,17 +3,10 @@ import re
 
 def verify(isbn):
     isbn_digits = isbn.replace("-", "")
-    if re.match(r"[[\d]{9}(X|\d)$", isbn_digits) is None:
-        return False
-    digits = zip(isbn_digits[:-1], range(10, 1, -1))
-    digits = [int(x) * i for x, i in digits]
-    checksum = (sum(digits) + (10 if isbn_digits[-1] == 'X' else int(isbn_digits[-1]))) % 11
-    if checksum != 0:
-        return False
-    return True
+    return is_valid_format(isbn_digits) and checksum(isbn_digits) == 0
 
 # validate ISBN-10 format - [\dX]*, 10 digits without hyphens
-def valid_digits_length(isbn_digits):
+def is_valid_format(isbn_digits):
     if re.match(r"[[\d]{9}(X|\d)$", isbn_digits) is None:
         return False
     return True
@@ -23,7 +16,6 @@ def valid_digits_length(isbn_digits):
 def checksum(isbn_digits):
     digits = zip(isbn_digits[:-1], range(10, 1, -1))
     digits = [int(x) * i for x, i in digits]
-    checksum = (sum(digits) + (10 if isbn_digits[-1] == 'X' else int(isbn_digits[-1]))) % 11
-
+    return (sum(digits) + (10 if isbn_digits[-1] == 'X' else int(isbn_digits[-1]))) % 11
 
 
